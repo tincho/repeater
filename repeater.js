720ac2a720a68577c0abc3d3d319a442a08f9966
tincho <martin.salinas@gmail.com>
2016-12-08 09:30:36 -0300

    helpful comments

--- repeater.js
+++ repeater.js
@@ -1,3 +1,7 @@
+/**
+ * from stack overflow
+ * a shortcut for non-native dom handling
+ */
 Element.prototype.appendAfter = function (element) {
     element.parentNode.insertBefore(this, element.nextSibling);
     return this;
@@ -5,13 +9,19 @@ Element.prototype.appendAfter = function (element) {
 
 [].forEach.call(document.querySelectorAll("[data-repeat]"), RepeatElement);
 
+/**
+ * core method
+ */
 function RepeatElement(rb) {
     var dataRepeat = rb.attributes.getNamedItem("data-repeat").value;
     var items = [];
     if (!isNaN(dataRepeat)) {
+        // create empty array with N elements that can be used with map
+        // (also from a stackoverflow answer!)
         items = Array.apply(null, Array(parseInt(dataRepeat)));
     } else {
         items = eval(preEval(dataRepeat));
+        // wrap in array if eval'd is plain value so we can always use map
         if(!Array.prototype.isPrototypeOf(items)) {
             items = [items];
         }
