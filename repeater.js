03a9cb5fb809f19d64c2aca684f20a5b4468861e
tincho <martin.salinas@gmail.com>
2016-12-08 10:15:58 -0300

    repeater iteration 4: extracting CreateElement and removing dependencies

--- repeater.js
+++ repeater.js
@@ -48,27 +48,33 @@ function RepeatElement(template) {
     var dataRepeat = template.attributes.getNamedItem("data-repeat").value;
     var items = [];
     if (!isNaN(dataRepeat)) {
-        // create empty array with N elements that can be used with map
+        // create empty array with N elements that can be used with map/reduce
         // (also from a stackoverflow answer!)
         items = Array.apply(null, Array(parseInt(dataRepeat)));
     } else {
         items = eval(dataRepeat.wrap('(',')'));
-        // wrap in array if eval'd is plain value so we can always use map
+        // wrap in array if eval'd is plain value so we can always use map/reduce
         if(!Array.prototype.isPrototypeOf(items)) {
             items = [items];
         }
     }
+    template.attributes.removeNamedItem('data-repeat');
 
-    var lastE = template;
-    var createdNodes = items.map(CreateElement);
+    CreateElement.template = template;
+    items.reduce(CreateElement, template);
     template.remove();
+};
 
-    function CreateElement(item) {
-        var newE = template.cloneNode(true);
-        if (typeof item !== 'undefined') {
-            newE.fillWith(item);
-        }
-        lastE = newE.appendAfter(lastE);
-        return lastE;
-    };
-}
+/**
+ * Creates element based in template (stored as self function property)
+ * designed to use with map/reduce in the context of RepeatElement
+ * but since has evolved and can be now used separately
+ */
+function CreateElement(after, data) {
+    var newE = CreateElement.template.cloneNode(true);
+    if (typeof data !== 'undefined') {
+        newE.fillWith(data);
+    }
+    newE.appendAfter(after);
+    return newE;
+};
