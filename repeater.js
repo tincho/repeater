6d6ba86628494465f9f21c1a1269638b5aefef64
tincho <martin.salinas@gmail.com>
2016-12-07 21:50:56 -0300

    repeater iteration 3: now accepts array of values/objects and fills content

--- repeater.js
+++ repeater.js
@@ -1,11 +1,7 @@
-// this is just a mockup tool
-// please tin dont waist your time trying to create a mini-angular
-
 Element.prototype.appendAfter = function (element) {
     element.parentNode.insertBefore(this, element.nextSibling);
     return this;
 };
-
 
 [].forEach.call(document.querySelectorAll("[data-repeat]"), RepeatElement);
 
@@ -13,16 +9,41 @@ function RepeatElement(rb) {
     var dataRepeat = rb.attributes.getNamedItem("data-repeat").value;
     var items = [];
     if (!isNaN(dataRepeat)) {
-        items = Array(parseInt(dataRepeat));
+        items = Array.apply(null, Array(parseInt(dataRepeat)));
+    } else {
+        items = eval(preEval(dataRepeat));
+        if(!Array.prototype.isPrototypeOf(items)) {
+            items = [items];
+        }
     }
 
     var lastb = rb;
-    var createdNodes = [].apply(null, items).map(function(item) {
+    var createdNodes = items.map(CreateElement);
+    rb.remove();
+
+    function CreateElement(item) {
         var newb = rb.cloneNode(true);
-        newb.attributes.removeNamedItem('data-repeat');
-        // in the future, do anything here with newb (item, lastb)
+        Fill(newb, item);
         lastb = newb.appendAfter(lastb);
         return lastb;
-    });
-    rb.remove();
+    };
+
+    function Fill(element, data) {
+        var content = element.innerHTML;
+        if (typeof data !== 'object') {
+            data = {'#': data};
+        }
+        Object.keys(data).forEach(function(k) {
+            var exp = new RegExp('\{\{' + k + '\}\}', 'g');
+            content = content.replace(exp, data[k]);
+        });
+        element.innerHTML = content;
+    };
+}
+
+/**
+ * wraps string in parenthesis if not already wrapped
+ */
+function preEval(str) {
+    return str.replace(/^([^\(])/,"($1").replace(/([^\)]$)/,"$1)");
 }
